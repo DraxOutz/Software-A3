acc7aaf32ce267ac202f6da54f1e02ea937c9d38
DraxOutz <kayquesacanaje@gmail.com>
2025-10-09 17:24:35 -0300

    Adiciona .gitignore e remove arquivos de segurança do Git

--- security/Password.java
+++
@@ -1,9 +0,0 @@
-package security;
-
-public class Password {
-    
-     public static final String PASSWORDEMAIL = "nric nhti ekrg aenk";
-     public static final String PASSWORDSQL =  "anima123";
-
-
-}
